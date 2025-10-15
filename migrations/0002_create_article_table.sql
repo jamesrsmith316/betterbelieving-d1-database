d87e5883294ebdc0b0c8ec00f0286752cfcd041c
Jim Smith <smithjim@gmail.com>
2025-10-14 23:56:04 -0400

    commenting out sql

--- migrations/0002_create_article_table.sql
+++ migrations/0002_create_article_table.sql
@@ -1,6 +1,7 @@
 -- Migration number: 0002 	 2025-07-06T22:04:18.794Z
 -- npx wrangler d1 execute example-db --remote --file=0002_create_article_table.sql
 -- npx wrangler d1 execute betterbelieving-d1 --remote --command "SELECT id,name,slug,category FROM article ORDER BY name;"
+/*
 CREATE TABLE IF NOT EXISTS [article] (
   id INTEGER PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
@@ -9,3 +10,4 @@ CREATE TABLE IF NOT EXISTS [article] (
   "content" blob, 
   "category" text
 )
+*/
